19d001d38c0cf1c7de01ebfad767d591ec751aa9
Conrado Buhrer <conrado@buhrer.net>
2019-01-15 10:00:53 -0200

    added commander

--- leave-im-channels.js
+++ leave-im-channels.js
@@ -1,30 +1,41 @@
-var WebClient = require('@slack/client').WebClient;
-var _ = require('lodash');
+var commander = require('commander')
+var SlackWebClient = require('@slack/client').WebClient
+var _ = require('lodash')
 
-var token = process.env.SLACK_BOT_TOKEN || '';
-var web = new WebClient(token);
+var token = process.env.SLACK_BOT_TOKEN || ''
+var slackweb = new SlackWebClient(token)
 
-var CHANNELS_TO_LEAVE = _.range(1228, 1275)
 
 function get_number(channel_name) {
-  var splitted = channel_name.split('im-');
+  var splitted = channel_name.split('im-')
   if(splitted.length == 2)
-    return parseInt(splitted[1]);
+    return parseInt(splitted[1])
   else
-    return -1;
+    return -1
 }
 
-web.channels.list(function(err, info) {
-  if (err) {
-    console.log('Error: ', err);
-  } else {
-    for(var i in info.channels) {
-      var channel_number = get_number(info.channels[i].name)
-      if (CHANNELS_TO_LEAVE.includes(channel_number)
-          && info.channels[i].is_member) {
-        web.channels.leave(info.channels[i].id)
-        console.log('Left: ', info.channels[i].name)
+function leave_channels(CHANNELS_TO_LEAVE) {
+  slackweb.channels.list(function(err, info) { if (err) {
+      console.log('Error: ', err)
+    } else {
+      for(var i in info.channels) {
+        var channel_number = get_number(info.channels[i].name)
+        if (CHANNELS_TO_LEAVE.includes(channel_number)
+            && info.channels[i].is_member) {
+          slackweb.channels.leave(info.channels[i].id)
+          console.log('Left: ', info.channels[i].name)
+        }
       }
     }
-  }
-})
+  })
+}
+
+commander
+  .option('-f, --from <n>', 'From channel (inclusive)')
+  .option('-t, --to <n>', 'To channel (exclusive)')
+  .version('0.0.2', '-v, --version')
+  .parse(process.argv)
+
+var CHANNELS_TO_LEAVE = _.range(commander.from, commander.to)
+
+leave_channels(CHANNELS_TO_LEAVE)
